908a403c71d9317b5caae88a15bd9fa4d9ef7c57
Vaibhav Seshadri <vaibhav.seshadri@needl.ai>
2024-01-29 21:30:54 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+1. pip install llama-index
+2. Start following llamaindex docs from here:
+   https://docs.llamaindex.ai/en/stable/getting_started/starter_example.html
---
+++ src/main.py
@@ -0,0 +1,18 @@
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
+import logging
+import sys
+
+# logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+# logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+documents = SimpleDirectoryReader("data").load_data()
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+
+query = input("What can I assist you with?\n")
+response = query_engine.query(query)
+print(response)
+
+query = input("Anything else?\n")
+response = query_engine.query(query)
+print(response)
