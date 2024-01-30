bbda4b6893acc7d84f02f25bcdc807a6ab47379f
Vaibhav Seshadri <vaibhav.seshadri@needl.ai>
2024-01-30 15:40:41 -0500

    Persist documents vector embeddings in storage folder

--- README.md
+++ README.md
@@ -4,13 +4,20 @@
 
 `python -m venv venv` <br>
 `source venv/bin/activate` (anytime you want to activate virtual environment) <br>
-      
+
 ## 2. Install LlamaIndex within virtual environment
-   `pip install llama-index`
-  
+
+`pip install llama-index`
+
 ## 3. Export environment variable `OPENAI_API_KEY` (store most recent key in API_key.txt)
 
 `export OPENAI_API_KEY=<PUBLIC_KEY>`
 
-## 4. Start following LlamaIndex docs from here:
-   https://docs.llamaindex.ai/en/stable/getting_started/starter_example.html
+## 4. Running code
+
+`cd src` <br>
+`python3 main.py` <br>
+
+# LlamaIndex documentation:
+
+https://docs.llamaindex.ai/en/stable/getting_started/starter_example.html
--- src/main.py
+++ src/main.py
@@ -1,23 +1,50 @@
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
+import os.path
+from llama_index import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
 import logging
 import sys
 
-# Uncomment these lines for logging
 
-# logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-# logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+def main():
+    # Uncomment these lines for logging
 
-documents = SimpleDirectoryReader("data").load_data()
-index = VectorStoreIndex.from_documents(documents)
-query_engine = index.as_query_engine()
+    # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    # logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
-print("\nHelios.ai mentor, type \"quit\" when you are done\n")
+    query_engine = generate_query_engine()
 
-while (True):
-    query = input(">> ")
+    print("\nHelios.ai mentor, type \"quit\" when you are done\n")
 
-    if query == "quit":
-        break
+    while (True):
+        query = input(">> ")
 
-    response = query_engine.query(query)
-    print("\n", response, "\n")
+        if query == "quit":
+            break
+
+        response = query_engine.query(query)
+        print("\n", response, "\n")
+
+
+def generate_query_engine():
+    # check if storage already exists
+    PERSIST_DIR = "../storage"
+    if not os.path.exists(PERSIST_DIR):
+        # load the documents and create the index
+        documents = SimpleDirectoryReader("../data").load_data()
+        index = VectorStoreIndex.from_documents(documents)
+        # store it for later
+        index.storage_context.persist(persist_dir=PERSIST_DIR)
+    else:
+        # load the existing index
+        storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+        index = load_index_from_storage(storage_context)
+
+    return index.as_query_engine()
+
+
+if __name__ == '__main__':
+    main()
