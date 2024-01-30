006c04468df599728f0901ea8e32688bd3becc63
Vaibhav Seshadri <vaibhav.seshadri@needl.ai>
2024-01-30 15:45:09 -0500

    minor edit

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ def main():
     # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     # logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
-    query_engine = generate_query_engine()
+    query_engine = create_query_engine()
 
     print("\nHelios.ai mentor, type \"quit\" when you are done\n")
 
@@ -29,15 +29,17 @@ def main():
         print("\n", response, "\n")
 
 
-def generate_query_engine():
+def create_query_engine():
     # check if storage already exists
     PERSIST_DIR = "../storage"
     if not os.path.exists(PERSIST_DIR):
         # load the documents and create the index
         documents = SimpleDirectoryReader("../data").load_data()
         index = VectorStoreIndex.from_documents(documents)
+
         # store it for later
         index.storage_context.persist(persist_dir=PERSIST_DIR)
+
     else:
         # load the existing index
         storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
